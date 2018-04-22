3d795fa3a00e50972bf44bd5bb28d95fb24c8e54
Tanapat Prateepthong <tanapat_pt@kkumail.com>
2018-04-22 21:30:21 +0700

    Update response

--- index.js
+++ index.js
@@ -20,114 +20,117 @@ app.get('/resetfail', function (req, res) {
   // setup email data with unicode symbols
   const mailOptions = {
     from: '"no-replay" <mailer.nmb@gmail.com>', // sender address
-    to:  data.receiver, // list of receivers
+    to: data.receiver, // list of receivers
     subject: `ระงับการใช้งานบัญชี`, // Subject line
     text: `บัญชีของคุณถูกระงับการใช้งาน เนื่องจากกรอกคำถามกันลืมผิดติดต่อกัน 3 ครั้ง กรุณาติดต่อผู้ดูแลระบบ`, // plain text body
     html: `<b>บัญชีของคุณถูกระงับการใช้งาน เนื่องจากกรอกคำถามกันลืมผิดติดต่อกัน 3 ครั้ง กรุณาติดต่อผู้ดูแลระบบ</b>` // html body
   };
 
   transporter.sendMail(mailOptions, function (err, info) {
-    if (err)
+    if (err) {
       console.log(err);
-    else
+      res.send(err);
+    } else {
       console.log(info);
-  });
-  console.log(req);
-  res.send('Reset failed.');
-});
-
-app.get('/resetsuccess', function (req, res) {
-  const data = req.query;
-  // create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'mailer.nmb@gmail.com',
-      pass: '$uper$ecret'
+      res.send(info);
     }
   });
 
-  // setup email data with unicode symbols
-  const mailOptions = {
-    from: '"no-replay" <mailer.nmb@gmail.com>', // sender address
-    to:  data.receiver, // list of receivers
-    subject: `เปลี่ยนแปลงรหัสผ่านบัญชี`, // Subject line
-    text: `บัญชีของคุณถูกเปลี่ยนรหัสผ่านการใช้งานเรียบร้อยแล้ว หากพบปัญหากรุณาติดต่อผู้ดูแลระบบ`, // plain text body
-    html: `<b>บัญชีของคุณถูกเปลี่ยนรหัสผ่านการใช้งานเรียบร้อยแล้ว หากพบปัญหากรุณาติดต่อผู้ดูแลระบบ</b>` // html body
-  };
+  app.get('/resetsuccess', function (req, res) {
+    const data = req.query;
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'mailer.nmb@gmail.com',
+        pass: '$uper$ecret'
+      }
+    });
 
-  transporter.sendMail(mailOptions, function (err, info) {
-    if (err)
-      console.log(err);
-    else
-      console.log(info);
-  });
-  console.log(req);
-  res.send('Reset Success.');
-});
+    // setup email data with unicode symbols
+    const mailOptions = {
+      from: '"no-replay" <mailer.nmb@gmail.com>', // sender address
+      to: data.receiver, // list of receivers
+      subject: `เปลี่ยนแปลงรหัสผ่านบัญชี`, // Subject line
+      text: `บัญชีของคุณถูกเปลี่ยนรหัสผ่านการใช้งานเรียบร้อยแล้ว หากพบปัญหากรุณาติดต่อผู้ดูแลระบบ`, // plain text body
+      html: `<b>บัญชีของคุณถูกเปลี่ยนรหัสผ่านการใช้งานเรียบร้อยแล้ว หากพบปัญหากรุณาติดต่อผู้ดูแลระบบ</b>` // html body
+    };
 
-app.get('/registerfail', function (req, res) {
-  const data = req.query;
-  // create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'mailer.nmb@gmail.com',
-      pass: '$uper$ecret'
-    }
-  });
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        console.log(info);
+        res.send(info);
+      }
+    });
 
-  // setup email data with unicode symbols
-  const mailOptions = {
-    from: '"no-replay" <mailer.nmb@gmail.com>', // sender address
-    to:  data.receiver, // list of receivers
-    subject: `การสมัครสมาชิกล้มเหลว`, // Subject line
-    text: `บัญชีของคุณถูกถูกตรวจสอบว่ามีข้อมูลไม่ถูกต้อง ซ้ำ หรือถูกแอบอ้าง กรุณาติดต่อผู้ดูแลระบบ`, // plain text body
-    html: `<b>บัญชีของคุณถูกถูกตรวจสอบว่ามีข้อมูลไม่ถูกต้อง ซ้ำ หรือถูกแอบอ้าง กรุณาติดต่อผู้ดูแลระบบ</b>` // html body
-  };
+    app.get('/registerfail', function (req, res) {
+      const data = req.query;
+      // create reusable transporter object using the default SMTP transport
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'mailer.nmb@gmail.com',
+          pass: '$uper$ecret'
+        }
+      });
 
-  transporter.sendMail(mailOptions, function (err, info) {
-    if (err)
-      console.log(err);
-    else
-      console.log(info);
-  });
-  console.log(req);
-  res.send('Register Failed.');
-});
+      // setup email data with unicode symbols
+      const mailOptions = {
+        from: '"no-replay" <mailer.nmb@gmail.com>', // sender address
+        to: data.receiver, // list of receivers
+        subject: `การสมัครสมาชิกล้มเหลว`, // Subject line
+        text: `บัญชีของคุณถูกถูกตรวจสอบว่ามีข้อมูลไม่ถูกต้อง ซ้ำ หรือถูกแอบอ้าง กรุณาติดต่อผู้ดูแลระบบ`, // plain text body
+        html: `<b>บัญชีของคุณถูกถูกตรวจสอบว่ามีข้อมูลไม่ถูกต้อง ซ้ำ หรือถูกแอบอ้าง กรุณาติดต่อผู้ดูแลระบบ</b>` // html body
+      };
 
-app.get('/registersuccess', function (req, res) {
-  const data = req.query;
-  // create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'mailer.nmb@gmail.com',
-      pass: '$uper$ecret'
-    }
-  });
+      transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        } else {
+          console.log(info);
+          res.send(info);
+        }
+      });
 
-  // setup email data with unicode symbols
-  const mailOptions = {
-    from: '"no-replay" <mailer.nmb@gmail.com>', // sender address
-    to:  data.receiver, // list of receivers
-    subject: `การสมัครสมาชิกเสร็จสมบูรณ์`, // Subject line
-    text: `บัญชีของคุณถูกถูกตรวจสอบและได้รับการยืนยันจากผู้ดูแลระบบแล้ว สามารถใช้งานในการเข้าสู่ระบบได้ หากพบปัญหากรุณาติดต่อผู้ดูแลระบบ`, // plain text body
-    html: `<b>บัญชีของคุณถูกถูกตรวจสอบและได้รับการยืนยันจากผู้ดูแลระบบแล้ว สามารถใช้งานในการเข้าสู่ระบบได้ หากพบปัญหากรุณาติดต่อผู้ดูแลระบบ</b>` // html body
-  };
+      app.get('/registersuccess', function (req, res) {
+        const data = req.query;
+        // create reusable transporter object using the default SMTP transport
+        const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'mailer.nmb@gmail.com',
+            pass: '$uper$ecret'
+          }
+        });
 
-  transporter.sendMail(mailOptions, function (err, info) {
-    if (err)
-      console.log(err);
-    else
-      console.log(info);
-  });
-  console.log(req);
-  res.send('Register Success.');
-});
+        // setup email data with unicode symbols
+        const mailOptions = {
+          from: '"no-replay" <mailer.nmb@gmail.com>', // sender address
+          to: data.receiver, // list of receivers
+          subject: `การสมัครสมาชิกเสร็จสมบูรณ์`, // Subject line
+          text: `บัญชีของคุณถูกถูกตรวจสอบและได้รับการยืนยันจากผู้ดูแลระบบแล้ว สามารถใช้งานในการเข้าสู่ระบบได้ หากพบปัญหากรุณาติดต่อผู้ดูแลระบบ`, // plain text body
+          html: `<b>บัญชีของคุณถูกถูกตรวจสอบและได้รับการยืนยันจากผู้ดูแลระบบแล้ว สามารถใช้งานในการเข้าสู่ระบบได้ หากพบปัญหากรุณาติดต่อผู้ดูแลระบบ</b>` // html body
+        };
 
-app.get('/', function (req, res) {
-  res.send("ระบบส่งอีเมลของ No More Bug powered by nodemailer");
-});
+        transporter.sendMail(mailOptions, function (err, info) {
+          if (err) {
+            console.log(err);
+            res.send(err);
+          } else {
+            console.log(info);
+            res.send(info);
+          }
+        });
+        console.log(req);
 
-app.listen(process.env.PORT || 8080, () => console.log('Mailer is running!'));
+      });
+
+      app.get('/', function (req, res) {
+        res.send("ระบบส่งอีเมลของ No More Bug powered by nodemailer");
+      });
+
+      app.listen(process.env.PORT || 8080, () => console.log('Mailer is running!'));
